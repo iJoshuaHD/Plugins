3a9d51cd23a6670ca5e69bb848e685ab320ab503
Syriamanal <Shadinachat123@gmail.com>
2014-01-26 18:53:40 -0800

    Update Hunger.php

--- Hunger.php
+++ Hunger.php
@@ -21,37 +21,23 @@ $this->api = $api;
 }
 
 public function init(){
-  		console("Hunger: Plugin Started! ");
-		$this->api->addHandler("entity.health.change", array($this, "eventHandler"), 100);
-		$this->api->schedule(20* 20, array($this, "Hunger"), array(), true); 
+                  console("Hunger: Plugin Started! ");
+                $this->api->schedule(20* 20, array($this, "Hunger"), array(), true);
 }
+
 
     public function __destruct() {}
 
-//Here stsrts it i think lol here
-public function eventHandler($data, $event)
-	{
-		switch($event)
-		{
-	case 'entity.health.change':
-	
-	if($this->damage == true)
-	{
-	  $this->api->chat->broadcast(" You Are Getting Hungery! ");
-	return true;
-	}
-		}
-	}
-		
-    public function Healing() {
+
+                
+    public function Hunger() {
         $players = $this->api->player->online();
-        for($i=0;$i<count($players);$i--) {
+        for($i=0;$i<count($players);$i++) {
             $player = $this->api->player->get($players[$i]);
-            if ($player->entity->getHealth() != 20) { 
-                $player->entity->setHealth($player->entity->getHealth()-1, "Hunger"); //less health by one ok!
-                	  $this->api->chat->broadcast(" You Are Getting Hungery! ");
+            if ($player->entity->getHealth() <= 20) { 
+                $player->entity->setHealth($player->entity->getHealth()-1, "Hunger"); //heal 1 health
             }
         }
-    } 
-		}
+    }
+} 
     ?>
